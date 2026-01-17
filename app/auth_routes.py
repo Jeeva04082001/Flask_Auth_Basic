8b08cb4de4c19cb026c138949a5d3ed368563bb6
jeeva <jeevae3112@gmail.com>
2026-01-17 21:19:31 +0530

    crud operation added

--- app/auth_routes.py
+++ app/auth_routes.py
@@ -25,6 +25,79 @@ def register():
 
     return jsonify({"message": "User Registerd"})
 
+
+
+@auth_bp.route("/users", methods=["GET"])
+def get_user_list():
+    users = User.query.all()
+    result=[
+        {
+            "id":u.id,
+            "email":u.email,
+        }
+        for u in users
+    ]
+    return jsonify(result),200
+
+@auth_bp.route("/user_by_id/<int:id>", methods=["GET"])
+def get_user_by_id(id):
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+    
+
+    print(user,'user----')
+
+    result = [
+        {
+            "id":user.id,
+            "email":user.email,
+            "password":user.password,
+            "role":user.role,
+        }
+        
+    ]
+    return jsonify(result),200
+
+
+
+@auth_bp.route("/update_user/<int:id>",methods=["PUT"])
+def update_user(id):
+    user = User.query.get(id)
+
+    if not user:
+        return jsonify({"error": "User not fount"}),404
+    
+    data = request.json
+
+    email = data.get("email")
+    role = data.get("role")
+
+    user.email = email,
+    user.role = role
+
+    db.session.commit()
+
+    return jsonify({"message":"User detail updated Successfully"})
+
+
+@auth_bp.route("delete_users/<int:id>", methods=["DELETE"])
+def delete_user(id):
+    user = User.query.get(id)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({"message": "User deleted"}), 200
+
+
+@auth_bp.errorhandler(404)
+def not_found(e):
+    return jsonify({"error": "Route not found"}), 404
+
+
 #Login
 @auth_bp.route("/login",methods=["POST"])
 def login():
